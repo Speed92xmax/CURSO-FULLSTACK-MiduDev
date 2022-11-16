4e7beef4fcd0b9e379ffd6dd3164519f57341df2
Kevin Pacheco <91635416+Speed92xmax@users.noreply.github.com>
2022-11-16 19:37:19 +0000

    Primer deploy con Heroku

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.use(logger);
 
 /* ASIGNACION DE PUERTOS Y ESCUCHA */
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
